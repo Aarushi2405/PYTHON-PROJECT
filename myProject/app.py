7f5df2eb2bb23adf5060fd390abae6ad389711a7
imt2018519 <rutvi1998@gmail.com>
2018-11-20 21:09:37 +0530

    Dashboard added

--- myProject/app.py
+++ myProject/app.py
@@ -13,12 +13,12 @@ Bootstrap(app)
 class LoginForm(FlaskForm):
 	username = StringField('username')
 	password = PasswordField('password')
-	remember = BooleanField('remember me')
 
 class RegisterForm(FlaskForm):
 	email = StringField('email', validators=[InputRequired(), Email(message='Invalid email'), Length(max=50)])
 	username = StringField('username', validators=[InputRequired(), Length(min=4, max=15)])
 	password = PasswordField('password', validators=[InputRequired(), Length(min=8, max=80)])
+
 
 @app.route('/')
 @app.route('/login', methods=['GET', 'POST'])
@@ -62,6 +62,9 @@ def signup():
 		conn.close()
 		return render_template('dashboard.html')
 	return render_template('signup.html', form=form)
+@app.route('/dashboard')
+def dashboard():
+	return render_template('dashboard.html')	
 
 if __name__=='__main__':
 	app.run(debug=True)
